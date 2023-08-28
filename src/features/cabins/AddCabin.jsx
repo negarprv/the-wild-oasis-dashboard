40843bcbba55f6a2052a52b5fd46c1cd68d96301
Negar Parvaresh <negar.prv@gmail.com>
2023-08-28 13:06:50 +0330

    modal created

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,26 @@
+import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
+import Button from "../../ui/Button";
+import CabinTable from "./CabinTable";
+
+function AddCabin() {
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  );
+}
+
+export default AddCabin;
